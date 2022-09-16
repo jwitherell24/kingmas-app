4cd5da5a04c24c2346050ada2b4f3473301658a9
jwitherell24 <witherellj24@gmail.com>
2022-09-15 21:15:35 -0400

    Created model for Product database

--- models.py
+++ models.py
@@ -12,3 +12,9 @@ Base = declarative_base()
 
 class Product(Base):
     __tablename__ = "products"
+    
+    product_id = Column(Integer, primary_key=True)
+    product_name = Column(String)
+    product_quantity = Column(Integer)
+    product_price = Column(Integer)
+    date_updated = Column(Date)
