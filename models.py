c6025fcb80bda186b2f25b0ab9a9bd79d2015edd
jwitherell24 <witherellj24@gmail.com>
2022-09-15 21:22:42 -0400

    Add __repr__ function

--- models.py
+++ models.py
@@ -14,7 +14,10 @@ class Product(Base):
     __tablename__ = "products"
     
     product_id = Column(Integer, primary_key=True)
-    product_name = Column(String)
-    product_quantity = Column(Integer)
-    product_price = Column(Integer)
-    date_updated = Column(Date)
+    product_name = Column("Name", String)
+    product_quantity = Column("Quantity", Integer)
+    product_price = Column("Price", Integer)
+    date_updated = Column("Updated", Date)
+    
+    def __repr__(self):
+        return f"Name: {self.product_name} Quantity: {self.product_quantity} Price: {self.product_price} Updated: {self.date_updated}"
